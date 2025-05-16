035042bb1a74b00f439a7bd950dcb591d8111e74
Sushobh <aryansushobh@gmail.com>
2025-05-16 21:28:39 +0530

    S*

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id(libs.plugins.android.application.get().pluginId)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
@@ -38,6 +38,10 @@ android {
         compose = true
     }
 }
+
+
+
+
 
 dependencies {
 
--- loggerplugin/build.gradle.kts
+++ loggerplugin/build.gradle.kts
@@ -12,10 +12,18 @@ kotlin {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
+gradlePlugin {
+    plugins {
+        create("loggerplugin") {
+            id = "com.sushobh.loggerplugin" // Optional if you use it by class
+            implementationClass = "com.sushobh.loggerplugin.ViewModelTransformerPlugin"
+        }
+    }
+}
 
 dependencies {
     implementation(gradleApi())
     implementation("org.ow2.asm:asm:9.5")
     implementation("org.ow2.asm:asm-commons:9.5")
-    implementation("com.android.tools.build:gradle:8.10.0")
+    implementation("com.android.tools.build:gradle:8.9.1")
 }
